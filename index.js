a5a3f8b20ba5454e4ecdd5e3b091edc19036d665
geovanecosta11 <123427618+geovanecosta11@users.noreply.github.com>
2023-02-02 22:22:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let pn = document.querySelector('#primeiro');
+let sn = document.querySelector('#outro');
+let resultado = document.querySelector('span');
+
+
+function soma(){
+   resultado.innerHTML = (pn.value+sn.value);
+}
